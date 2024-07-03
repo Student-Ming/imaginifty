080e2712bd7d3c7484b6a2309eccac0759d05b70
studentming <3294946159@qq.com>
2024-07-03 19:47:54 +0800

    dev(通用配置): 解决生产环境tw不生效的问题

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./out/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
