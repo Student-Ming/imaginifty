d8382d9358acfea9fc6df449520787b87458ae5e
studentming <3294946159@qq.com>
2024-07-04 18:28:37 +0800

    dev(通用配置): 解决GitHub Pages样式不生效问题

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/out' : '',
+  basePath: process.env.NODE_ENV === 'production' ? 'https://student-ming.github.io/imaginifty/' : '',
   exportTrailingSlash: true,
 };
 
