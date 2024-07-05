79e914eebc242a6490d44f7418a41085aecb9835
studentming <3294946159@qq.com>
2024-07-05 16:50:03 +0800

    dev(通用配置): 解决GitHub Pages上tw样式不生效的问题

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   output: 'export',
   basePath: process.env.NODE_ENV === 'production' ? '/out' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://student-ming.github.io/imaginifty/' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://student-ming.github.io/imaginifty/out/' : '',
   exportTrailingSlash: true,
 };
 
