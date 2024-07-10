16ff31f3e773bd269db65a9403ae8d0c41791c06
我不是峰哥 <120102594+Student-Ming@users.noreply.github.com>
2024-07-10 11:29:39 +0800

    dev(路径找回): 解决favicon图标路径问题 (#36)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href={process.env.NODE_ENV === 'production' ? 'https://student-ming.github.io/imaginifty/favicon.ico' : '/favicon.ico'} />
+      </Head>
       <body>
         <Main />
         <NextScript />
