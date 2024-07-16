9af8fa4734d77ac5ff4b50ccfc52f52414f8249b
studentming <3294946159@qq.com>
2024-07-16 19:36:48 +0800

    dev(主题切换): 优化主题切换开关刷新后失效的问题

--- src/components/global/navbar/ThemeSwitcher.tsx
+++ src/components/global/navbar/ThemeSwitcher.tsx
@@ -1,9 +1,15 @@
 import { useTheme } from "next-themes";
 import { Sun, MoonStar } from "lucide-react";
 import { Switch, Tooltip } from "@nextui-org/react";
+import { useEffect, useState } from "react";
 
 export const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme()
+  const [isSelected, setSelected] = useState(false)
+
+  useEffect(() => {
+    theme === 'dark' ? setSelected(true) : setSelected(false)
+  }, [theme])
 
   return (
     <div className="mt-1">
@@ -14,6 +20,7 @@ export const ThemeSwitcher = () => {
         color={theme === 'light' ? 'default' : 'secondary'}
         offset={-45}>
         <Switch
+          isSelected={isSelected}
           size="md"
           color="secondary"
           startContent={<MoonStar />}
