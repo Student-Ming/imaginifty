a25824af1adfc5d693452858cf3e030a81933a23
我不是峰哥 <120102594+Student-Ming@users.noreply.github.com>
2024-07-25 17:41:59 +0800

    dev(体验优化): 解决移动端能放大的问题 (#44)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1,minimum-scale=1, maximum-scale=1, user-scalable=no"></meta>
         <link rel="icon" href={process.env.NODE_ENV === 'production' ? 'https://student-ming.github.io/imaginifty/favicon.ico' : '/favicon.ico'} />
       </Head>
       <body>
